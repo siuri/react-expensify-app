123a447048596835c6b4cd219119fef32af5eacc
Jung Hyun Kim <siurilove@jungs-mbp.attlocal.net>
2017-12-26 23:18:42 -0600

    Show not showing number of expenses in filters

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,14 +5,18 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, unFiltersExpenseCount }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const unFiltersExpenseWord = unFiltersExpenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpenesTotal = numeral(expensesTotal / 100).format('$0x.00');
 
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header-title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpenesTotal}</span></h1>
+                { unFiltersExpenseCount > 0 &&
+                <div className="page-header-subtitle">Not showing <span>{unFiltersExpenseCount}</span> {unFiltersExpenseWord} because of filters </div>
+                }
                 <div className="page-header__sections">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -20,13 +24,14 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
         </div>
     );
 };
-
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
         expenseCount: visibleExpenses.length,
+        unFiltersExpenseCount: state.expenses.length - visibleExpenses.length,
         expensesTotal: selectExpensesTotal(visibleExpenses)
     };
 };
+
 
 export default connect(mapStateToProps)(ExpensesSummary);
